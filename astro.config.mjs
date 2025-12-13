70ad3f5bc8a2fe9500a00912160c40fe6e7f525a
wroobi <wojciech.wroblewski@hotmail.com>
2025-12-13 20:31:18 +0100

    lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
 
 import cloudflare from "@astrojs/cloudflare";
 
