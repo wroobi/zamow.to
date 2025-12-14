c50691211f72b6d2a4732ec967c7bc6d85ea697a
wroobi <wojciech.wroblewski@hotmail.com>
2025-12-14 10:18:02 +0100

    duplicated code

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -46,13 +46,3 @@ export const createSupabaseServerInstance = (context: {
 
   return supabase;
 };
-import { createClient, type SupabaseClient } from "@supabase/supabase-js";
-
-import type { Database } from "../db/database.types.ts";
-
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
-
-export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
-
-export type { SupabaseClient, Database };
